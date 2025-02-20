043b12afc7c5599cd19d5b0553926f7e892d43df
M-Biban <k22037403@kcl.ac.uk>
2025-02-20 15:44:34 +0000

    add tutorial routes to index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,20 +2,24 @@ import Vue from "vue";
 import Router from "vue-router";
 import TutorialPage from "@/views/TutorialsPage.vue";
 import TutPage1 from "@/views/TutPage1.vue";
+import Tutorials from "@/store/initial-tut-states";
+
+const tutorialRoutes = Object.values(Tutorials).map((tutorial) => ({
+    path: tutorial.url,
+    name: tutorial.name,
+    componet: TutPage1,
+    props: { tutorialData: tutorial},
+}));
 
 Vue.use(Router);
 
 export default new Router({
     routes: [
+        ...tutorialRoutes,
         {
             path: "/tutorials",
             name: "TutorialPage",
             component: TutorialPage, // This will point to the component you want to show for this route
-        },
-        {
-            path: "/tut1",
-            name: "Tut1",
-            component: TutPage1,
         },
     ],
 });
